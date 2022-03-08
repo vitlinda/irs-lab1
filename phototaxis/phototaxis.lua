caa84b392e5d6d33574381b1c66a26615d2bbc33
vitlinda <linda.vitali2@studio.unibo.it>
2022-03-08 17:09:28 +0100

    add extra time in phototaxis example

--- phototaxis/phototaxis.lua
+++ phototaxis/phototaxis.lua
@@ -1,6 +1,8 @@
 -- Put your global variables here
 MAX_VELOCITY = 10
+MOVE_STEPS = 15
 
+n_steps = 0
 
 --[[ This function is executed every time you press the 'execute'
      button ]]
@@ -8,45 +10,48 @@ function init()
 	left_v = robot.random.uniform(0,MAX_VELOCITY)
 	right_v = robot.random.uniform(0,MAX_VELOCITY)
 	robot.wheels.set_velocity(left_v,right_v)
-
+	n_steps = 0
 end
 
 
 --[[ This function is executed at each time step
      It must contain the logic of your controller ]]
 function step()
+	n_steps = n_steps + 1
+	if n_steps % MOVE_STEPS == 0 then
 
-	light_front = robot.light[1].value + robot.light[24].value
-	light_left = robot.light[6].value + robot.light[7].value
-	light_right = robot.light[18].value + robot.light[19].value
-	light_back = robot.light[12].value + robot.light[13].value
+		light_front = robot.light[1].value + robot.light[24].value
+		light_left = robot.light[6].value + robot.light[7].value
+		light_right = robot.light[18].value + robot.light[19].value
+		light_back = robot.light[12].value + robot.light[13].value
 
-	log("robot.light_front = " .. light_front)
-	log("robot.light_left = " .. light_left)
-	log("robot.light_right = " .. light_right)
-	log("robot.light_back = " .. light_back)
+		log("robot.light_front = " .. light_front)
+		log("robot.light_left = " .. light_left)
+		log("robot.light_right = " .. light_right)
+		log("robot.light_back = " .. light_back)
 
-	if light_front > 0 then
-		left_v = MAX_VELOCITY
-		right_v = MAX_VELOCITY
+		if light_front > 0 then
+			left_v = MAX_VELOCITY
+			right_v = MAX_VELOCITY
+		end
+
+		if  light_left > 0 then
+			left_v = -1
+			right = MAX_VELOCITY
+		end
+
+		if light_right > 0 then
+			left_v = MAX_VELOCITY
+			right_v = -1
+		end
+
+		if light_back > 0 then
+			left_v = MAX_VELOCITY
+			right_v = -1
+		end
+
+		robot.wheels.set_velocity(left_v,right_v)
 	end
-
-	if  light_left > 0 then
-		left_v = -1
-		right = MAX_VELOCITY
-	end
-
-	if light_right > 0 then
-		left_v = MAX_VELOCITY
-		right_v = -1
-	end
-
-	if light_back > 0 then
-		left_v = MAX_VELOCITY
-		right_v = -1
-	end
-
-	robot.wheels.set_velocity(left_v,right_v)
 
 end
 
@@ -61,7 +66,7 @@ function reset()
 	left_v = robot.random.uniform(0,MAX_VELOCITY)
 	right_v = robot.random.uniform(0,MAX_VELOCITY)
 	robot.wheels.set_velocity(left_v,right_v)
-
+	n_steps = 0
 end
 
 
